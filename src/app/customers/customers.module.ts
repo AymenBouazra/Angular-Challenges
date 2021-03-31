48cce73894430bc451575410c105c3e15cfbfdf7
AymenBouazra <Aymenbouazra994@gmail.com>
2021-03-31 10:48:09 +0100

    5th commit

---
+++ src/app/customers/customers.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CustomersRoutingModule } from './customers-routing.module';
+import { CustomersComponent } from './customers.component';
+import { AddingProductComponent } from './components/adding-product/adding-product.component';
+import { UpdatingProductComponent } from './components/updating-product/updating-product.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [CustomersComponent, AddingProductComponent, UpdatingProductComponent],
+  imports: [
+    CommonModule,
+    CustomersRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+  ]
+})
+export class CustomersModule { }
